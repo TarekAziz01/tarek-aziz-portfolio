1fb6103be79cf413f112019e082138c2d87813cc
Tarek Aziz <mdaziz3289@gmail.com>
2024-01-06 20:56:02 +0600

    font secondary update

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -37,10 +37,10 @@ const Services = () => {
         <div className="flex flex-col lg:flex-row ">
           {/* text & img */}
           <div className="flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0 ">
-            <h2 className="font-primary text-[32px] mb-6 tracking-[10%] uppercase text-accent">
+            <h2 className="font-secondary text-[32px] mb-6 tracking-[10%] uppercase text-accent">
               what I do.
             </h2>
-            <h3 className="text-white font-primary font-semibold text-[26px] leading-[46px] max-w-[455px] mb-16">
+            <h3 className="text-white font-secondary font-semibold text-[26px] leading-[46px] max-w-[455px] mb-16">
               I am a Front-end Developer with one years of experience.
             </h3>
             <button className="btn h-[48px] px-6 text-lg rounded-full font-primary text-white">
@@ -58,7 +58,7 @@ const Services = () => {
                     key={index}
                   >
                     <div className="max-w-[476px]">
-                      <h4 className="text-white text-[24px] tracking-wider font-primary font-semibold mb-6">
+                      <h4 className="text-white text-[24px] tracking-wider font-secondary font-semibold mb-6">
                         {name}
                       </h4>
                       <p className="text-white font-secondary leading-tight tracking-wider">
@@ -74,7 +74,7 @@ const Services = () => {
                       </a>
                       <a
                         href=" #"
-                        className="bg-gradient-to-r text-transparent bg-clip-text from-[#42A6E3] to-[#FF56F6] font-bold font-primary hover:from-[#FF56F6] hover:to-[#42A6E3] text-lg"
+                        className="bg-gradient-to-r text-transparent bg-clip-text from-[#42A6E3] to-[#FF56F6] font-bold font-secondary hover:from-[#FF56F6] hover:to-[#42A6E3] text-lg"
                       >
                         {link}
                       </a>
